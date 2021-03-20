eff5880cecd81b40aa4d621269b1b50c4d1eedac
gabrieleal <gabriel.leal.00@hotmail.com>
2021-03-20 11:53:41 -0300

    completamos CRUD

--- src/movies.js
+++ src/movies.js
@@ -1,34 +1,66 @@
 const { Router } = require("express");
 const router = Router();
-
+const _ = require("underscore");
 let json = require("../mov.json");
+const { route } = require("./routes/rutas");
 
-console.log(json); 
+console.log(json);
 
-router.get("/",(req,res) =>{
+router.get("/", (req, res) => {
     res.json(json);
 });
 
-router.post("/",(req,res)=>{
+router.post("/", (req, res) => {
     // req.body objeto por el cual el servidor recibe los datos
     console.log(req.body);
     //almacenamos las en las varibales los datos enviados al servidor
-    let {name, year, rating } = req.body;
+    let { name, year, rating } = req.body;
 
     //validamos que no sean nulos
-    if(name && year && rating){
-        var id = json.length + 1;
+    if (name && year && rating) {
+        var id = json.length;
         // con ...req.body almacenamos en el objeto todo lo obtenido en el servidor
         //como segundo parametro establecemos el Id con el caul alamcenamos el dato
-        var newMovie={...req.body, id};
+        var newMovie = { ...req.body, id };
         json.push(newMovie);
         //enviamos las peliculas actualizadas
         res.json(json);
-    }else{
+    } else {
         //definimos el codigo de estado y el mensaje
         res.status(500).send("ocurrio ub error interno");
     }
-    
+
+});
+
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, year, rating } = req.body;
+    if (name && year && rating) {
+        _.each(json, (pel, i) => {
+            if (pel.id == id) {
+                pel.name = name;
+                pel.year = year;
+                pel.rating = rating;
+            }
+        });
+        res.json(json);
+    }else {
+        //definimos el codigo de estado y el mensaje
+        res.status(500).send("ocurrio ub error interno");
+    }
+
+});
+
+router.delete("/:id", (req, res) => {
+    let { id } = req.params;
+    // each(arreglo, (peliculatemporal,indice))
+    _.each(json, (pel, i) => {
+        if (pel.id == id) {
+            // splice(indice,cantidad) de peliculas que quiero eliminar
+            json.splice(i, 1);
+        }
+    });
+    res.send("deleted");
 });
 
 module.exports = router;
